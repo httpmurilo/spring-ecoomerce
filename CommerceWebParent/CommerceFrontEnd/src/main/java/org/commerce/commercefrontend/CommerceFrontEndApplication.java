4d38f0e1cb99ddc973871d599b7c13b4c257b7cb
httpmurilo <muriloeduardo1997@outlook.com>
2021-09-03 20:02:50 -0300

    projetos dependentes

---
+++ CommerceWebParent/CommerceFrontEnd/src/main/java/org/commerce/commercefrontend/CommerceFrontEndApplication.java
@@ -0,0 +1,13 @@
+package org.commerce.commercefrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceFrontEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceFrontEndApplication.class, args);
+    }
+
+}
