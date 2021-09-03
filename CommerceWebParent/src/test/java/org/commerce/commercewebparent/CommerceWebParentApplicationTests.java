8ea9cd6be967ea5e1c030f4acf405be614eb2d87
httpmurilo <muriloeduardo1997@outlook.com>
2021-09-03 19:27:01 -0300

    projetos

---
+++ CommerceWebParent/src/test/java/org/commerce/commercewebparent/CommerceWebParentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.commerce.commercewebparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceWebParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
