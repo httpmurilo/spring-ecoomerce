d74f1a36bc7099d5b2fb23a12c5829cc7bcee09e
Murilo Eduardo Saldanha R de Melo <murilo.melo@softplan.com.br>
2021-11-30 21:04:48 -0300

    extras-security

--- CommerceWebParent/CommerceBackEnd/src/test/java/org/commerce/commercebackend/admin/user/RoleRepositoryTests.java
+++ CommerceWebParent/CommerceBackEnd/src/test/java/org/commerce/commercebackend/admin/user/RoleRepositoryTests.java
@@ -2,12 +2,12 @@ package org.commerce.commercebackend.admin.user;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.commerce.common.entity.Role;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
+import org.commerce.common.entity.Role;
 
 import java.util.List;
 
